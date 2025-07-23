f48fd8ac693d34c26e58c104aa41258b43d11696
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2025-07-22 20:03:43 -0700

    all tool examples

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import { handleMessage } from './messages/handler.js'
 const messages = [
   {
     role: 'user',
-    content: 'What is 72 divided by 9?',
+    content: 'What is 2 + 2?',
+  },
+  {
+    role: 'user',
+    content: 'What time is it?',
+  },
+  {
+    role: 'user',
+    content: 'Can you roll a die for me?',
   },
 ]
 
@@ -13,7 +21,7 @@ const main = async () => {
   for (const message of messages) {
     console.log('[USER]', message.content)
     const response = await handleMessage(message)
-    console.log('[LLM Response]', response)
+    console.log('[LLM Response]', response, '\n\n============\n')
   }
 }
 
